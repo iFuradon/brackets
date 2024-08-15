f6cb95685c40317cc265c60be4a3cc3c3ce34361
iFuradon <iFuradon@gmail.com>
2024-08-15 21:42:35 +0400

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let stack =[];
+  let brackets = new Map(bracketsConfig);
+  let openBrackets = new Set(bracketsConfig.map(pair => pair[0]));
+  
+  for (let i=0; i < str.length; i++){
+    
+    let currentBracket = str[i];
+    
+    if (brackets.has(currentBracket)) {
+      if (stack.length > 0 && brackets.get(stack[stack.length - 1]) === currentBracket){
+        stack.pop();
+      } else {
+        stack.push(currentBracket);
+      }
+    } else {
+      let lastOpenedBracket = stack.pop();
+      if (brackets.get(lastOpenedBracket) !== currentBracket){
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
